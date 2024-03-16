ce95974d04f45d88c6eddea08bd31e9fd199e5d2
dakkk <mikeylay@126.com>
2024-03-16 19:06:17 +0800

    增加一个GitTest的java代码文件到master分支上

---
+++ web01/src/com/dakkk/GitTest.java
@@ -0,0 +1,16 @@
+package com.dakkk;
+
+/**
+ * ClassName: GitTest
+ * Package: com.dakkk
+ *
+ * @Create 2024/3/16 18:58
+ * @Version 1.0
+ * @Author dakkk
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git test");
+    }
+}
